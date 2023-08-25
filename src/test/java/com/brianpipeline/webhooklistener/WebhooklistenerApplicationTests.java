9f4927150ae9b4cfa9abe60f29fa8798fa49cd38
Brian Kurilko <briankurilko@gmail.com>
2023-08-25 15:24:19 -0400

    First commit

---
+++ src/test/java/com/brianpipeline/webhooklistener/WebhooklistenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianpipeline.webhooklistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhooklistenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
